134cf6e1c69b759e4783cca5f0b43d457a80b188
AvaneeshThautam <avaneesh.t@gmail.com>
2016-12-30 20:53:20 +0100

    add stringtoint

--- avan_code.py
+++ avan_code.py
@@ -1,2 +1,11 @@
-import math
-# some code i am going to put in.
+
+test_string = '2 5 6 7 123 252'
+
+print(test_string)
+
+print(stringtoint(test_string))
+
+def stringtoint(input_string):
+    list_form = int(input_string.split())
+    return list_form
+
