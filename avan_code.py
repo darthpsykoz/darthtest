ef72feec898d00ebff72003b7da8e1306b23c3af
AvaneeshThautam <avaneesh.t@gmail.com>
2016-12-30 20:58:31 +0100

    correct strtoint

--- avan_code.py
+++ avan_code.py
@@ -1,11 +1,15 @@
+def stringtoint(input_string):
+    num_list = []
+    for i in input_string.split():
+        num_list.append(int(i))
+    print(type(num_list), type(num_list[1]))
+    return num_list
 
+#def inttodec(input_int):
 test_string = '2 5 6 7 123 252'
 
 print(test_string)
 
+
+
 print(stringtoint(test_string))
-
-def stringtoint(input_string):
-    list_form = int(input_string.split())
-    return list_form
-
